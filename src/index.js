9cba6c1520094882cde9142ecd3fdaea9c6d003f
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2024-12-09 18:25:42 +0000

    Estudo sobre middlewares com uso do multer

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const uploadMiddleware = require('./middlewares/upload')
+const app = express()
+
+app.use(express.static('public'))
+
+
+app.post('/upload', uploadMiddleware.single('image'), (req, res) => {
+  console.log({ a: req.middlewareA, b: req.middlewareB })
+  console.log(req.file, req.body)
+  res.end()
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}/`))
